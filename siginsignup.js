35e40c3e5572da11345e0e5901f30c2b9b52a00f
doaaleyna <71530094+doaaleyna@users.noreply.github.com>
2022-04-30 03:20:21 +0300

    SignİnSignUp Kısımları düzenlendi

--- siginsignup.js
+++ siginsignup.js
@@ -10,3 +10,16 @@ sign_up_btn.addEventListener('click',()=>{
 sign_in_btn.addEventListener('click',()=>{
     container.classList.remove('sign-up-mode');
 });
+
+function show(){
+    var pswrd=document.getElementById('pswrd');
+    var icon=document.querySelector('.fas');
+    if(pswrd.type === "password"){
+        pswrd.type = "text";
+        pswrd.style.marginTop = "20px";
+        icon.style.color="#ff7200";
+    }else{
+        pswrd.type="password";
+        icon.style.color="grey";
+    }
+}
